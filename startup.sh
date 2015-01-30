32a762ae48c59641ecddf7ea201bf89b3fc55843
Ekin Koc <ekin@eknkc.com>
2015-01-30 02:33:47 +0200

    Config update

--- startup.sh
+++ startup.sh
@@ -9,8 +9,5 @@ chmod +x /etc/rc.d/rc.local
 echo "echo 511 > /proc/sys/net/core/somaxconn" >> /etc/rc.d/rc.local
 echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/rc.d/rc.local
 
-mkdir -p /data/redis/server
-mkdir -p /data/redis/sentinel
-
-docker run --restart=on-failure --net host -v /data/redis/server:/data -d onedio/redis
-docker run --restart=on-failure --net host -v /data/redis/sentinel:/data -d onedio/redis redis-server /redis/sentinel.conf --sentinel
+docker run --restart always --net host -v /data:/data -d onedio/redis
+docker run --restart always --net host -d onedio/redis redis-server /redis/sentinel.conf --sentinel
